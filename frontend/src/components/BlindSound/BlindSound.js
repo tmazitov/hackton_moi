aa355c4ce861ca78cb4417ed2dc2517645c79d6b
RuslanNartdinov <nartdinovruslan2020@gmail.com>
2024-05-15 21:12:49 +0400

    bug fix

--- frontend/src/components/BlindSound/BlindSound.js
+++ frontend/src/components/BlindSound/BlindSound.js
@@ -4,7 +4,7 @@ import "./BlindSound.scss"
 const BlindSound = () => {
 	return (
 		<div className='blind-sound'>
-			<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 48 48"><defs><mask id="ipTSquare0"><path fill="#555" stroke="#fff" stroke-width="4" d="M39 6H9a3 3 0 0 0-3 3v30a3 3 0 0 0 3 3h30a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3Z"/></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipTSquare0)"/></svg>
+			<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 48 48"><defs><mask id="ipTSquare0"><path fill="#555" stroke="#fff" strokeWidth="4" d="M39 6H9a3 3 0 0 0-3 3v30a3 3 0 0 0 3 3h30a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3Z"/></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipTSquare0)"/></svg>
 		</div>
 	);
 };
