804aad0b739e0d7e44b233180dc7aaa8d5df3537
RuslanNartdinov <nartdinovruslan2020@gmail.com>
2024-05-15 18:03:52 +0400

    bug fixed

--- frontend/src/components/FeaturePopUp/FeaturePopUp.js
+++ frontend/src/components/FeaturePopUp/FeaturePopUp.js
@@ -2,6 +2,9 @@ import React from 'react';
 import "./FeaturePopUp.scss"
 
 const FeaturePopUp = (props) => {
+	const [search, setSearch] = React.useState('');
+	console.log(search)
+	//setSearch(props.searchValue);
 	React.useEffect(()=>{
 		const element = document.getElementById(props.el_id)
 		const button = document.getElementById("button")
@@ -9,7 +12,7 @@ const FeaturePopUp = (props) => {
 		const windowListen = (event)=>{
 			console.log(event.target);
 
-			if((event.target !== element) && (element.classList.contains("feature-popup_true")))
+			if(((event.target !== element) && (element.classList.contains("feature-popup_true"))) && !element.contains(event.target))
 				{
 					console.log("lol")
 
@@ -19,6 +22,7 @@ const FeaturePopUp = (props) => {
 			else if ((event.target === button) | (event.target === button_search)) {
 				element.classList.add("feature-popup_true")
 				element.parentElement.classList.add("shadow_active")
+				setSearch(props.searchValue)
 			}
 		}
 		document.addEventListener("click", windowListen)
@@ -29,9 +33,17 @@ const FeaturePopUp = (props) => {
 	return (
 		<div className="shadow">
 
-		<div id={props.el_id} className='feature-popup_false'>
-			
-		</div>
+			<div id={props.el_id} className='feature-popup_false'>
+				<div className="feature-popup__search">
+					<div className="search_container">
+						<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="black" d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396l1.414-1.414l-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8s3.589 8 8 8m0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6s-6-2.691-6-6s2.691-6 6-6"/></svg>
+						<input value={search} onChange={(e)=>{setSearch(e.target.value)}} type="text" className="search_input" />
+					</div>
+				</div>
+				<div className="feature-popup__results">
+
+				</div>
+			</div>
 		</div>
 	);
 };
--- frontend/src/components/Navigation/NavButton/NavButton.js
+++ frontend/src/components/Navigation/NavButton/NavButton.js
@@ -6,7 +6,7 @@ const NavButton = (props) => {
 	const navigate = useNavigate();
 	return (
 		<div onClick={()=>{navigate(props.linkTo)}} className='nav-button'>
-			<div className="nav-button_icon"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 48 48"><defs><mask id="ipTSquare0"><path fill="#555" stroke="#fff" stroke-width="4" d="M39 6H9a3 3 0 0 0-3 3v30a3 3 0 0 0 3 3h30a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3Z"/></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipTSquare0)"/></svg></div>
+			<div className="nav-button_icon"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 48 48"><defs><mask id="ipTSquare0"><path fill="#555" stroke="#fff" strokeWidth="4" d="M39 6H9a3 3 0 0 0-3 3v30a3 3 0 0 0 3 3h30a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3Z"/></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipTSquare0)"/></svg></div>
 			<div className="nav-button_name">{props.name.toUpperCase()}</div>
 		</div>
 	);
